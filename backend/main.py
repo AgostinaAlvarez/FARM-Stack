6cd82e2d8bb252ddec944e8c7e32dacecf529e75
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2023-12-23 13:32:25 -0300

    sdd

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+#parte del midellware
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ['http://localhost:5173'],
