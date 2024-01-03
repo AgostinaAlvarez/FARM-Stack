d0feb22d98579b71f0aef688914d9a5020c36736
AgostinaAlvarez <agosalvarezz1999@gmail.com>
2024-01-03 18:31:56 -0300

    change

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,15 @@
-
-
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:pin420608@localhost:3306/bd"
+load_dotenv()
+
+uri_db = os.getenv("URI_APP")
+
+
+SQLALCHEMY_DATABASE_URL = uri_db
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
